3aafc26f1b0df9deb1ddcbb6da9a8ded458a22ec
swaraj saxena <swarishraj00@gmail.com>
2023-05-08 12:00:45 +0530

    nav fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,17 +2,41 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
-	return <div className='p-4 flex items-center gap-4 w-full max-w-[1440px]'>
-    <img src="./logo.svg" width={20} alt="" srcset="" />
-    <div className='text-1xl font-extrabold tracking-tight'>NAUTISTIC XD</div>
+	return (
+		<div className='flex items-center w-full justify-center bg-[#0b1121]/50 backdrop-blur-lg z-50 sticky top-0 border-b-2 border-[#94A3B8]/5'>
+			<div className='p-4 py-6 flex items-center gap-4 w-full max-w-[1440px]  '>
+				<img src='./logo.svg' width={20} alt='' srcset='' />
+				<div className='text-1xl font-extrabold tracking-tight'>NAUTISTIC XD</div>
 
-    <div className='flex gap-4 items-center ml-auto'>
-      <Link className='font-light hover:text-blueMain capitalize tracking-wider transition-all' to={'/'}>home</Link>
-      <Link className='font-light hover:text-blueMain capitalize tracking-wider transition-all' to={'/test'}>test</Link>
-      <Link className='font-light hover:text-blueMain capitalize tracking-wider transition-all' to={'/statistics'}>statistics</Link>
-      <Link className='font-light hover:text-blueMain capitalize tracking-wider transition-all' to={'/login'}>login</Link>
-    </div>
-  </div>;
+				<div className='flex gap-4 items-center ml-auto'>
+					<Link
+						className='font-light hover:text-blueMain capitalize tracking-wider transition-all'
+						to={'/'}
+					>
+						home
+					</Link>
+					<Link
+						className='font-light hover:text-blueMain capitalize tracking-wider transition-all'
+						to={'/test'}
+					>
+						test
+					</Link>
+					<Link
+						className='font-light hover:text-blueMain capitalize tracking-wider transition-all'
+						to={'/statistics'}
+					>
+						statistics
+					</Link>
+					<Link
+						className='font-light hover:text-blueMain capitalize tracking-wider transition-all'
+						to={'/login'}
+					>
+						login
+					</Link>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Navbar;
